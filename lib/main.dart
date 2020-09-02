c020cfcdc6e5523faea228118274ac90994bd1b7
Maycon Santos <maycon.s.santos44@gmail.com>
2020-09-02 12:01:21 -0300

    Projeto finalizado

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)))),
+    );
+  }
+}
